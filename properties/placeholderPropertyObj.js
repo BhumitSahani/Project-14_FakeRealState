026f9015b254dd0aac2287da91e01f1a050035c7
Bhumit <geetaumesh1632@gmail.com>
2026-08-04 20:27:19 +0530

    Update image path in placeholderPropertyObj.js

--- properties/placeholderPropertyObj.js
+++ properties/placeholderPropertyObj.js
@@ -3,7 +3,7 @@ const placeholderPropertyObj = [{
     priceGBP: '100000',
     roomsM2: [16,12,6,11,5],
     comment: 'This is the description',
-    image: 'house5.jpg'
+    image: 'houseImages/house5.jpg'
 }]
 
 export {placeholderPropertyObj}
