304501f96c0335e2b2c996568fdd416b2c3225d3
Bhumit <geetaumesh1632@gmail.com>
2026-08-04 19:52:41 +0530

    Add files via upload

---
+++ properties/placeholderPropertyObj.js
@@ -0,0 +1,9 @@
+const placeholderPropertyObj = [{
+    propertyLocation: 'Joe Bloggs Street',
+    priceGBP: '100000',
+    roomsM2: [16,12,6,11,5],
+    comment: 'This is the description',
+    image: 'house5.jpg'
+}]
+
+export {placeholderPropertyObj}
---
+++ properties/propertyForSaleArr.js
@@ -0,0 +1,32 @@
+const propertyForSaleArr = [
+    {
+        propertyLocation: 'Kensington, London',
+        priceGBP: '890000',
+        roomsM2: [14,18,14,10,6],
+        comment: 'Highly desirable location in stunning scenery:',
+        image: 'house1.jpg'
+    },
+    {
+        propertyLocation: 'Wirral, Liverpool',
+        priceGBP: '650000',
+        roomsM2: [18, 16, 15, 14, 17, 19, 9, 8],
+        comment: 'Astonishing view with a modern finish:',
+        image: 'house2.jpg'
+    },
+    {
+        propertyLocation: 'Beach, Brighton',
+        priceGBP: '420000',
+        roomsM2: [5,14,13,10],
+        comment: 'Beautiful interior and a spacious room.:',
+        image: 'house3.jpg'
+    },
+    {
+        propertyLocation: 'Highlands, Scotland',
+        priceGBP: '550000',
+        roomsM2: [10, 9, 11, 4],
+        comment: 'Lots of potential, snug, a must see!:',
+        image: 'house4.jpg'
+    }
+]
+
+export {propertyForSaleArr}
